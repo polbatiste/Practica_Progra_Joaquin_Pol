93c17bafc6f5decef0839e3b81b87f50ca6238e0
joaquindemierfernandezcaro <quinodemier3@gmail.com>
2024-10-29 14:37:39 -0400

    Registrar el router de dueños en el servidor principal

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,61 +1,6 @@
-import shutil
-import io
-import pandas as pd
-from typing import List
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel as PydanticBaseModel
+from fastapi import FastAPI
+from clientes.endpoints.endpoints import router as clientes_router
 
-# Clases existentes de ejemplo para la funcionalidad de contratos
-class BaseModel(PydanticBaseModel):
-    class Config:
-        arbitrary_types_allowed = True
+app = FastAPI()
 
-class Contrato(BaseModel):
-    fecha: str
-    centro_seccion: str
-    nreg: str
-    nexp: str
-    objeto: str
-    tipo: str
-    procedimiento: str
-    numlicit: str
-    numinvitcurs: str
-    proc_adjud: str
-    presupuesto_con_iva: str
-    valor_estimado: str
-    importe_adj_con_iva: str
-    adjuducatario: str
-    fecha_formalizacion: str
-    I_G: str
-
-class ListadoContratos(BaseModel):
-    contratos = List[Contrato]
-
-# Instancia principal de la aplicación FastAPI
-app = FastAPI(
-    title="Gestión de Clínica Veterinaria",
-    description="""API para la gestión de datos de la clínica veterinaria y otras funcionalidades.""",
-    version="0.2.0",
-)
-
-# Endpoint para recuperar datos de contratos (funcionalidad existente)
-@app.get("/retrieve_data/")
-def retrieve_data():
-    todosmisdatos = pd.read_csv('./contratos_inscritos_simplificado_2023.csv', sep=';')
-    todosmisdatos = todosmisdatos.fillna(0)
-    todosmisdatosdict = todosmisdatos.to_dict(orient='records')
-    listado = ListadoContratos()
-    listado.contratos = todosmisdatosdict
-    return listado
-
-# Endpoint para envío de formularios (funcionalidad existente)
-class FormData(BaseModel):
-    date: str
-    description: str
-    option: str
-    amount: float
-
-@app.post("/envio/")
-async def submit_form(data: FormData):
-    return {"message": "Formulario recibido", "data": data}
+app.include_router(clientes_router, prefix="/api")
