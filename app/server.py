9772a26c5f7e8d5d4eafd721e79041987488eb12
joaquindemierfernandezcaro <quinodemier3@gmail.com>
2024-11-22 09:51:56 -0500

    extras de server.py

--- app/server.py
+++ app/server.py
@@ -1,3 +1,6 @@
+# server.py
+
+
 import pandas as pd
 from typing import List
 from fastapi import FastAPI, HTTPException
@@ -26,28 +29,6 @@ app.include_router(appointments_router, prefix="/api/v1")
 app.include_router(animals_router, prefix="/api/v1")
 app.include_router(tratamientos_router, prefix="/api/v1")
 app.include_router(productos_router, prefix="/api/v1")
-
-# Endpoint para recuperar datos de contratos
-@app.get("/retrieve_data/")
-def retrieve_data():
-    try:
-        contratos_df = pd.read_csv('./contratos_inscritos_simplificado_2023.csv', sep=';').fillna(0)
-        contratos_dict = contratos_df.to_dict(orient='records')
-        return ListadoContratos(contratos=contratos_dict)
-    except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="Archivo de contratos no encontrado")
-
-# Modelo de datos para envío de formularios
-class FormData(PydanticBaseModel):
-    date: str
-    description: str
-    option: str
-    amount: float
-
-# Endpoint para recibir y procesar un formulario
-@app.post("/envio/")
-async def submit_form(data: FormData):
-    return {"message": "Formulario recibido", "data": data}
 
 # Mensaje de bienvenida
 @app.get("/")
